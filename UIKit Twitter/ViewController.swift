431844d3f31c1aa2cb3feffd8109b2b86fb7bf80
khawlah-kh <44741544+khawlah-kh@users.noreply.github.com>
2022-02-05 12:25:27 +0300

    Initial Commit

---
+++ UIKit Twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit Twitter
+//
+//  Created by khawlah khalid on 05/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
